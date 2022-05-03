1a73e8c7f0992d730fe659c3debf9afe23daa1e9
Humberto A. Ale Perez <humbertoale89@googlemail.com>
2022-05-03 15:47:36 +0200

    Add Asteroids Controller
    -Removed the default template Weather controller.
    -Created new Asteroids Controller.
    -Added Nugget RestSharp for handle HTTPClient request to NASA API
    -Added NUgget NewtonsoftASP for handle the responses and deserialize.
    -Tested Controller function as well url tru Swagger support.

---
+++ AtmiraTest/Controllers/AsteroidsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace AtmiraTest.Controllers
+{
+    [Route("asteroids")]
+    [ApiController]
+    public class AsteroidsController : ControllerBase
+    {
+        [HttpGet("{planet}")]
+        public async Task<IActionResult> Asteroids([FromRoute] string planet)
+        {
+            var startDate = DateTime.Now.ToString("yyyy-MM-dd");
+            var client = new RestClient($"https://api.nasa.gov/neo/rest/v1/feed?start_date={startDate}&api_key=DEMO_KEY");
+            RestResponse response = await client.ExecuteAsync(new RestRequest());
+            string jsonObj = response.Content;
+            var deserializedObj = JObject.Parse(jsonObj);
+            return Ok();            
+        }
+    }
+}
