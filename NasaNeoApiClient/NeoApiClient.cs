c5215bf30f1b3b251bbd2ffd942fe4e8a050717a
Humberto A. Ale Perez <humbertoale89@googlemail.com>
2022-05-03 22:32:42 +0200

    add api client
    -Add Api client class library

---
+++ NasaNeoApiClient/NeoApiClient.cs
@@ -0,0 +1,38 @@
+﻿using RestSharp;
+
+namespace NasaNeoApiClient
+{
+    public interface INeoApiClient{}
+    public class NeoApiClient : INeoApiClient
+    {
+        private string urlBase = "https://api.nasa.gov/neo/rest/v1/";
+        private string apiKey = "DEMO_KEY";
+        public NeoApiClient()
+        {
+
+        }
+        public async Task<string> getFeedRange(string begin, string? end)
+        {
+            Dictionary<string, string> headers = new Dictionary<string, string>();
+            headers.Add("startDate", begin);
+            if (!string.IsNullOrEmpty(end))
+            {
+                headers.Add("endDate", end);
+            }
+            string jsonResponse = await requestApi("feed", headers);
+            return jsonResponse;
+        }
+
+        private async Task<string> requestApi(string endpoint, Dictionary<string,string> paramSet)
+        {
+            string paramsStr = "";
+            foreach (KeyValuePair<string, string> param in paramSet)
+            {
+                paramsStr += paramsStr.Length > 0 ? "&" : "" + param.Key + "=" + param.Value;
+            }
+            var apiClient = new RestClient($"{urlBase}{endpoint}?{paramsStr}&api_key={apiKey}");
+            RestResponse apiResponse = await apiClient.ExecuteAsync(new RestRequest());
+            return apiResponse.Content;
+        }
+    }
+}
